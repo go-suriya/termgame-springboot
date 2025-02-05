a06fdc4bcbcd984caa6bb339f6ffcf0ca6f37fbe
Pakphom Promsean <mcast31mcast31@gmail.com>
2025-02-06 00:40:47 +0700

    update flyway

--- src/main/resources/db/migration/V1__create_user_table.sql
+++ src/main/resources/db/migration/V1__create_user_table.sql
@@ -1,6 +1,9 @@
 CREATE TABLE IF NOT EXISTS "user" (
     id SERIAL PRIMARY KEY,
+    username VARCHAR(255) UNIQUE NOT NULL,
+    password VARCHAR(255) NOT NULL,
     first_name VARCHAR(255) NOT NULL,
     last_name VARCHAR(255) NOT NULL,
-    phone_number VARCHAR(10) UNIQUE
+    phone_number VARCHAR(10) UNIQUE,
+    point decimal(10,2) NOT NULL DEFAULT 0.00
 );
