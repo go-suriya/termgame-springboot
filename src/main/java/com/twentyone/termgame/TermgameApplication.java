f2fdee9faac6c63d1053dd36570ee9082a18973c
Pakphom Promsean <mcast31mcast31@gmail.com>
2025-02-05 22:48:40 +0700

    first commit

---
+++ src/main/java/com/twentyone/termgame/TermgameApplication.java
@@ -0,0 +1,13 @@
+package com.twentyone.termgame;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TermgameApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TermgameApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/twentyone/termgame/TermgameApplicationTests.java
@@ -0,0 +1,13 @@
+package com.twentyone.termgame;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TermgameApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
